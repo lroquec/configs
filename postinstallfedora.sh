996d8bf679d1efa4b4e2b61f158bc67edf1f7664
Laura Roque <laura.roque@eurona.com>
2022-12-15 20:06:00 +0100

    error correction

--- postinstallfedora.sh
+++ postinstallfedora.sh
@@ -9,18 +9,18 @@ fwupdmgr update
 mkdir -p /usr/share/fonts
 
 echo "Install Media codecs"
-dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-dnf groupupdate sound-and-video
-dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
-dnf install lame\* --exclude=lame-devel
-dnf group upgrade --with-optional Multimedia
-dnf install ffmpeg ffmpeg-libs libva libva-utils
+dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf groupupdate -y sound-and-video
+dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
+dnf install -y lame\* --exclude=lame-devel
+dnf group upgrade -y --with-optional Multimedia
+dnf install -y ffmpeg ffmpeg-libs libva libva-utils
 
 echo "Changing hostname"
 hostnamectl set-hostname evilqueen
 
 echo "Install grub customizer. Please add mitigations=off"
-dnf install grub-customizer
+dnf install -y grub-customizer
 
 
 
